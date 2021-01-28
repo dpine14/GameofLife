e4b293ad1b23727f7ffab0a3e4f27722db6990b6
Dylan.Pine <dylan.pine115@gmail.com>
2021-01-27 19:44:26 -0700

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+#Game of life function and helpers
+#Dylan Pine 
+import numpy as np
+
+x=100
+y=100
+universe = 
+
+function generate_universe(x,y):
+    universe = np.zeros((x,y), dtype='bool')
+
+
+function game_of_life(x,y, universe):
+
